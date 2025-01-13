8b87807292780a56772802ba28ee91cba68a17d6
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2025-01-13 15:57:44 -0300

    added more DateTime utils

--- lib/extension/date_time.dart
+++ lib/extension/date_time.dart
@@ -1,8 +1,16 @@
 import 'package:intl/intl.dart';
 
+const humanReadableDateTimeFormat = "yyyy-LLL-dd (E) HH:mm";
+
 final humanReadableDateFormatMap = <String, DateFormat>{};
 
-String humanReadableDateTimeFormat = "yyyy-LLL-dd (E) HH:mm";
+final iso8601TimeZoneOffsetHourFormat = NumberFormat(
+  "+00;-00",
+);
+
+final iso8601TimeZoneOffsetMinuteFormat = NumberFormat(
+  "00",
+);
 
 extension DateTimeExtension on DateTime {
   /// [locale] is `en` for English, `pt` for Portuguese etc.
@@ -12,6 +20,33 @@ extension DateTimeExtension on DateTime {
       locale.getHumanReadableDateFormat().format(
             this,
           );
+
+  String toISO8601TimeZoneString() {
+    final timeZoneOffsetInMinutes = timeZoneOffset.inMinutes;
+
+    var hour = timeZoneOffsetInMinutes ~/ 60;
+    var minute = timeZoneOffsetInMinutes % 60;
+
+    return "${iso8601TimeZoneOffsetHourFormat.format(
+      hour,
+    )}:${iso8601TimeZoneOffsetMinuteFormat.format(
+      minute,
+    )}";
+  }
+}
+
+extension DateTimeNullableExtension on DateTime? {
+  String? toISO8601WithTimeZoneString() {
+    if (this == null) {
+      return null;
+    }
+
+    return DateFormat(
+      "${this!.toIso8601String()}${this!.toISO8601TimeZoneString()}",
+    ).format(
+      this!,
+    );
+  }
 }
 
 extension DateTimeStringExtension on String {
