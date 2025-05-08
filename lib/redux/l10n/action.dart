eedb9a2d7c6b0f1977ac8d8ca2e696f05b47bee8
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2025-05-08 15:11:34 -0300

    added l10n action

---
+++ lib/redux/l10n/action.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_guiritter/common/common.import.dart'
+    as common_gui_ritter show AppLocalizationsGuiRitter, l10nGuiRitter;
+import 'package:flutter_guiritter/model/model.import.dart' show StateModel;
+import 'package:redux/redux.dart' show Store;
+import 'package:redux_thunk/redux_thunk.dart' show ThunkAction;
+
+ThunkAction<StateModel<AppLocalizationsLocalType>>
+    setL10n<AppLocalizationsLocalType>({
+  required AppLocalizationsLocalType? l10n,
+  required common_gui_ritter.AppLocalizationsGuiRitter? l10nGuiRitter,
+}) =>
+        (
+          Store<StateModel<AppLocalizationsLocalType>> store,
+        ) async {
+          common_gui_ritter.l10nGuiRitter = l10nGuiRitter;
+
+          return store.dispatch(
+            L10nAction<AppLocalizationsLocalType>(
+              l10n: l10n,
+              l10nGuiRitter: l10nGuiRitter,
+            ),
+          );
+        };
+
+class L10nAction<AppLocalizationsLocalType> {
+  final AppLocalizationsLocalType? l10n;
+  final common_gui_ritter.AppLocalizationsGuiRitter? l10nGuiRitter;
+
+  const L10nAction({
+    required this.l10n,
+    required this.l10nGuiRitter,
+  });
+}
--- lib/redux/redux.import.dart
+++ lib/redux/redux.import.dart
@@ -1,1 +1,3 @@
+export 'package:flutter_guiritter/redux/l10n/action.dart'
+    show setL10n, L10nAction;
 export 'package:flutter_guiritter/redux/main.reducer.dart' show dispatch;
