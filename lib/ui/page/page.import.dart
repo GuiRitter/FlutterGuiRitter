4df912caf6ec62da1d8b3195ae3a3643dce0ac1c
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2025-05-21 16:23:59 -0300

    added library splash page

--- lib/ui/page/page.import.dart
+++ lib/ui/page/page.import.dart
@@ -1,2 +1,3 @@
 export 'package:flutter_guiritter/ui/page/loading.page.dart' show LoadingPage;
 export 'package:flutter_guiritter/ui/page/sign_in.page.dart' show SignInPage;
+export 'package:flutter_guiritter/ui/page/splash.page.dart' show SplashPage;
